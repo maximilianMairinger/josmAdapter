8ec562cba762340d9e0172e4a26633bb443955bb
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2024-10-11 22:15:03 +0200

    add browser export

---
+++ app/src/josmAdapterBrowserAggregate.ts
@@ -0,0 +1,54 @@
+export { 
+  Adapter,  
+  isAdapterSym,
+  fullyConnectedJosmAdapter,
+  makeAdapterPair
+} from "./fullyConnectedAdapter"
+
+export { 
+  dataBaseToAdapter, 
+  unifyDataAndDataBase
+} from "./dataBaseAdapter"
+
+export {
+  eventListenerToAdapter,
+  josmEventReflection
+} from "./domEventReflection"
+
+export { 
+  makeJosmReflection,
+  josmReflection
+} from "./josmReflection"
+
+export { 
+  scrollTargetToAdapter,
+  josmScrollReflection
+} from "./domScrollReflection"
+
+export { 
+  domEditAbleReflection,
+  josmEditAbleReflection
+} from "./domEditAbleReflection"
+
+
+export {
+  localStorageToAdapter,
+  josmLocalStorageReflection
+} from "./localStorageReflection"
+
+export {
+  workerJosmAdapterServer,
+  workerJosmAdapterClient,
+  workerToAdapter
+} from "./workerAdapter"
+
+export {
+  websocketJosmAdapterServer,
+  websocketJosmAdapterClient,
+  wsToAdapter
+} from "./wsAdapter"
+
+export {
+  reflectionToSaniTemplate,
+  restrictAdapter
+} from "./restrictAdapter"
