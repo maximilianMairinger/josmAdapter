eb1c7265bf363fc9624d7f8634047a43741ea140
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2025-09-16 23:48:33 +0200

    fix reflection if init value is not set yet so undefined

--- app/src/josmReflection.ts
+++ app/src/josmReflection.ts
@@ -49,8 +49,10 @@ export function josmReflection(reflectionAdapter: PrimaryTransmissionAdapter | S
       adapter.send(storedData)
     }
 
-    if (db[instanceTypeSym] === "Data" && (typeof storedData === "object" && storedData !== null)) throw new Error("Reflection is object be should be primitive based on default.")
-    if (db[instanceTypeSym] === "DataBase" && !(typeof storedData === "object" && storedData !== null)) throw new Error("Reflection is primitive be should be object based on default.")
+    if (storedData !== undefined) {
+      if (db[instanceTypeSym] === "Data" && (typeof storedData === "object" && storedData !== null)) throw new Error("Reflection is object be should be primitive based on default.")
+      if (db[instanceTypeSym] === "DataBase" && !(typeof storedData === "object" && storedData !== null)) throw new Error("Reflection is primitive be should be object based on default.")
+    }
 
 
     p.res(pVal = {db, adapter})
